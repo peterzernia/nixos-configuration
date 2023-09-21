b5f7528fcc7c461b9f55cbcb3c3e4738f9ff9df5
System administrator <peter@peterzernia.com>
2023-09-21 11:49:36 +0200

    add user.nix

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,7 @@
       ./boot.nix
       ./hardware-configuration.nix
       ./media.nix
+      ./user.nix
     ];
 
   hardware.opengl.driSupport32Bit = true;
@@ -96,45 +97,11 @@
     pulse.enable = true;
   };
 
-  programs.fish.enable = true;
-  environment.variables.EDITOR = "nvim";
-
   # Keyring daemon for storing credentials. Required by NextCloud desktop client.
   services.gnome.gnome-keyring.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.peter = {
-    isNormalUser = true;
-    description = "Peter";
-    extraGroups = [ "networkmanager" "wheel" ];
-    openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGvDG+MlgB0jKXNGqIcRFMbHbOI7j5SL8BFoMI7IkTt/JRGND9VpZF0gm6mvUs/7XRw7nlkfzBgRORUmo+ECioRQTLYGKnbsO7qOwNMK1vn/uEN0Fm8aYV9ZmFJ8rA8KruMk13iHQ4mxfSel5+l3UGM0FNCoYP1KH3sGVhlIbSgSvDFOtxdtSyIaPGWi8V14bo/FA+cHjczCEi9QtQoxQI623SoXT33TrZexscBM2sMjWVoPvhPHAY3jNzh9/kUxCl7wh+5YSKRd1fa+c/GYX59UETYhL903miId+i1VsI0lPQ0mhwJl6XpwzmGZJTjoDiTasyIJxGVRKOB1g47bdk27FaCYLP2+b/IfN4Zh+YRQ+fvMoQ53b5PsJLfj3Zfo9fzqcji77EUHoa0lMgzw0Z0zhgMA9GjslBx4Qijs4xKkROFrh1XBBjwqMHzG+boxMXRE6go1JVa2tpMEfrrSh0rd7A6oh6CiyRSyfBt6svlSkjgQhLv9TslRRqFY7nQRk= peterzernia@FIN-0309-Peter-Zernia.fritz.box"];
-    shell = pkgs.fish;
-    packages = with pkgs; [
-      discord
-      feh
-      ffmpeg
-      firefox
-      heroic
-      libreoffice
-      mullvad-vpn
-      nextcloud-client
-      qbittorrent
-      sc-controller
-      soulseekqt
-      spotify
-      steam
-      syncthing
-      vlc
-    ];
-  };
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -151,21 +118,6 @@
   fonts.fonts = with pkgs; [
     (nerdfonts.override { fonts = [ "DroidSansMono" ]; })
   ];
-
-  services = {
-    syncthing = {
-        enable = true;
-        user = "peter";
-        dataDir = "/home/peter/Sync";
-        configDir = "/home/peter/Sync/.config";
-    };
-    mullvad-vpn = {
-      enable = true;
-    };
-    openssh = {
-      enable = true;
-    };
-  };
 
   system.stateVersion = "22.11";
 }
---
+++ user.nix
@@ -0,0 +1,51 @@
+{ pkgs, ... }:
+
+{
+  users.users.peter = {
+    isNormalUser = true;
+    description = "Peter";
+    extraGroups = [ "networkmanager" "wheel" ];
+    openssh.authorizedKeys.keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGvDG+MlgB0jKXNGqIcRFMbHbOI7j5SL8BFoMI7IkTt/JRGND9VpZF0gm6mvUs/7XRw7nlkfzBgRORUmo+ECioRQTLYGKnbsO7qOwNMK1vn/uEN0Fm8aYV9ZmFJ8rA8KruMk13iHQ4mxfSel5+l3UGM0FNCoYP1KH3sGVhlIbSgSvDFOtxdtSyIaPGWi8V14bo/FA+cHjczCEi9QtQoxQI623SoXT33TrZexscBM2sMjWVoPvhPHAY3jNzh9/kUxCl7wh+5YSKRd1fa+c/GYX59UETYhL903miId+i1VsI0lPQ0mhwJl6XpwzmGZJTjoDiTasyIJxGVRKOB1g47bdk27FaCYLP2+b/IfN4Zh+YRQ+fvMoQ53b5PsJLfj3Zfo9fzqcji77EUHoa0lMgzw0Z0zhgMA9GjslBx4Qijs4xKkROFrh1XBBjwqMHzG+boxMXRE6go1JVa2tpMEfrrSh0rd7A6oh6CiyRSyfBt6svlSkjgQhLv9TslRRqFY7nQRk= peterzernia@FIN-0309-Peter-Zernia.fritz.box"];
+    shell = pkgs.fish;
+    packages = with pkgs; [
+      discord
+      feh
+      ffmpeg
+      firefox
+      heroic
+      libreoffice
+      mullvad-vpn
+      nextcloud-client
+      qbittorrent
+      sc-controller
+      soulseekqt
+      spotify
+      steam
+      syncthing
+      vlc
+    ];
+  };
+
+  services = {
+    syncthing = {
+        enable = true;
+        user = "peter";
+        dataDir = "/home/peter/Sync";
+        configDir = "/home/peter/Sync/.config";
+    };
+    mullvad-vpn = {
+      enable = true;
+    };
+    openssh = {
+      enable = true;
+    };
+  };
+
+  environment.variables.EDITOR = "nvim";
+  programs.fish.enable = true;
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+}
