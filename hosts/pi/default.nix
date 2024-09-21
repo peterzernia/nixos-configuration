5394ab0b93e45c303e4003ae376f2fddc0403d13
Peter <peter@peterzernia.com>
2024-09-21 21:40:31 +0000

    organize pi

--- hosts/pi/default.nix
+++ hosts/pi/default.nix
@@ -3,30 +3,15 @@
 {
   imports = 
   [
+    ./boot.nix
+    ./hardware.nix
+
     ../../modules/common
     ../../modules/networking
     ../../modules/user
   ];
 
-  boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
-    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
-    loader = {
-      grub.enable = false;
-      generic-extlinux-compatible.enable = true;
-    };
-  };
 
   desktopEnv.enable = false;
   hostname = "pi";
-
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
-  };
-
-  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 }
---
+++ hosts/pi/hardware.nix
@@ -0,0 +1,13 @@
+{ lib, ... };
+
+{
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+}
