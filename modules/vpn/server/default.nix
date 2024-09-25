e0ecc60f67bb6f8740b18a6585ea763b76d813af
Peter <peter@peterzernia.com>
2024-09-25 16:36:59 +0000

    update vpn

--- modules/vpn/server/default.nix
+++ modules/vpn/server/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   networking.firewall.allowedTCPPorts = [ 51820 ];
@@ -6,7 +6,17 @@
 
   networking.wireguard.interfaces = {
     wg0 = {
+      ips = [ "10.100.0.1/24" ];
+
       listenPort = 51820;
+
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o end0 -j MASQUERADE
+      '';
+
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o end0 -j MASQUERADE
+      '';
 
       privateKeyFile = "/etc/wireguard/privatekey";
 
@@ -14,12 +24,12 @@
         {
           # pixel
           publicKey = "mOzQewwzqLDvfiVjkCiboWfdvzjT9cw0O4O4NvbPQVc=";
-          allowedIPs = [ "10.0.0.2/32" ];
+          allowedIPs = [ "10.100.0.2/32" ];
         }
         {
           # m1
           publicKey = "pm/MJIAAKF/29PBMhtTdOPC4eHp37+gMF45R3SVrSgQ=";
-          allowedIPs = [ "10.0.0.3/32" ];
+          allowedIPs = [ "10.100.0.3/32" ];
         }
       ];
     };
@@ -31,7 +41,4 @@
     externalInterface = "end0";
     internalInterfaces = [ "wg0" ];
   };
-
-  # handle DNS resolution
-  systemd.services.systemd-resolved.enable = true;
 }
