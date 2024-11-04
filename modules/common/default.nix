90f4a298f01dd850bbd96a2f4d488180ca051108
Peter <peter@peterzernia.com>
2024-11-04 08:55:02 +0100

    set automatic gc

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -1,5 +1,12 @@
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
   system.stateVersion = "24";
 }
