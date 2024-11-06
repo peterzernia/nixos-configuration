1f6c2d598b3d3a59e4e25790b46149f3276f64be
Peter <peter@peterzernia.com>
2024-11-06 17:19:15 +0100

    remove gc

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -2,11 +2,5 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
 
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 14d";
-  };
-
   system.stateVersion = "24";
 }
