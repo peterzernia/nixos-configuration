9f14bf0b67bd5de82cb690a255884e1644a48356
Peter <peter@peterzernia.com>
2025-03-03 12:39:33 +0100

    fix wireguard postdown

--- modules/vpn/client/default.nix
+++ modules/vpn/client/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   networking = {
@@ -10,5 +10,11 @@
         };
       };
     };
+  };
+  # https://github.com/NixOS/nixpkgs/issues/228055#issuecomment-1839337654
+  system.activationScripts = {
+    fix-wireguard-activation = ''
+      ${pkgs.iproute2}/bin/ip link del wg0
+     '';
   };
 }
