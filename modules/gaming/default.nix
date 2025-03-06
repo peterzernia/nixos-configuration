c851e11f9a4e51654dd931fcd8efbd662ba736b8
Peter <peter@peterzernia.com>
2025-03-06 16:41:06 +0100

    add sc controller config

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -1,3 +1,5 @@
+{ config, ... }:
+
 {
   imports =
     [
@@ -19,4 +21,15 @@
   };
 
   programs.gamemode.enable = true;
+
+  # steam controller
+  hardware.steam-hardware.enable = true;
+  users.groups.steam-controller = { };
+  users.users.${config.user}.extraGroups = [ "steam-controller" ];
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTRS{idVendor}=="28de", GROUP="steam-controller", MODE="0660"
+    KERNEL=="hidraw*", KERNELS=="*28DE:*", GROUP="steam-controller", MODE="0660"
+    KERNEL=="uinput", MODE="0660", GROUP="steam-controller", OPTIONS+="static_node=uinput"
+  '';
+  boot.kernelModules = [ "uinput" ];
 }
