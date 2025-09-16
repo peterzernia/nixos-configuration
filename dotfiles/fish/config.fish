8c7264fe095f871ccb1238a79bac7b65aaf744da
Peter <peter@peterzernia.com>
2025-09-16 09:45:33 +0200

    auto start tmux

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,8 +1,8 @@
-# if status is-interactive
-# and not set -q TMUX
-#   tmux new-session -A -s default || tmux new-session -s default
-# end
-#
+if status is-interactive
+and not set -q TMUX
+  tmux new-session -A -s default || tmux new-session -s default
+end
+
 bind -M insert \cf accept-autosuggestion
 
 # atuin
--- modules/media/default.nix
+++ modules/media/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  seedingPort = 33882;
+  seedingPort = 60255;
 in
 {
   # sonarr
