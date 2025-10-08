cd8d825b1ff1f6d5bec2c08e36a9dd761a4fe95c
peterzernia <peter.zernia@fincompare.com>
2025-10-08 19:52:36 +0200

    add key

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -13,11 +13,13 @@
 
   users.users.${config.user} = {
     isNormalUser = true;
-    description = "Peter";
+    description = "peter";
     extraGroups = [ "networkmanager" "wheel" "media" "video" "input" ];
     openssh.authorizedKeys.keys = [
       # m1
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDGvDG+MlgB0jKXNGqIcRFMbHbOI7j5SL8BFoMI7IkTt/JRGND9VpZF0gm6mvUs/7XRw7nlkfzBgRORUmo+ECioRQTLYGKnbsO7qOwNMK1vn/uEN0Fm8aYV9ZmFJ8rA8KruMk13iHQ4mxfSel5+l3UGM0FNCoYP1KH3sGVhlIbSgSvDFOtxdtSyIaPGWi8V14bo/FA+cHjczCEi9QtQoxQI623SoXT33TrZexscBM2sMjWVoPvhPHAY3jNzh9/kUxCl7wh+5YSKRd1fa+c/GYX59UETYhL903miId+i1VsI0lPQ0mhwJl6XpwzmGZJTjoDiTasyIJxGVRKOB1g47bdk27FaCYLP2+b/IfN4Zh+YRQ+fvMoQ53b5PsJLfj3Zfo9fzqcji77EUHoa0lMgzw0Z0zhgMA9GjslBx4Qijs4xKkROFrh1XBBjwqMHzG+boxMXRE6go1JVa2tpMEfrrSh0rd7A6oh6CiyRSyfBt6svlSkjgQhLv9TslRRqFY7nQRk= peterzernia@FIN-0309-Peter-Zernia.fritz.box"
+      # m3
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB2qwaWgyHL8juWDNF1413f4xqoFDKy4guarLYlOoUaB peterzernia@MacBook-Pro-von-FinCompare.local"
     ];
   };
 
