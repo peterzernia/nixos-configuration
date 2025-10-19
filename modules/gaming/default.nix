d1da197cc503e91c5c5b792d8b5bed70a1e38ccc
peter <peter@peterzernia.com>
2025-10-19 20:04:29 +0200

    update steam settings

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -16,8 +16,14 @@
       heroic
       mangohud
       sc-controller
-      steam
     ];
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
   };
 
   programs.gamemode.enable = true;
